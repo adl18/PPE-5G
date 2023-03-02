194cdd3f27fbed8b6b9ea10b0adb7c209e53a653
adl18 <126814288+adl18@users.noreply.github.com>
2023-03-02 19:20:01 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,10 @@
+//let numberOfDogs = 4; //créer une variable
+let debit = document.getElementById('debit');
+let mot = document.getElementById('mot');
+
+console.log('coucou');
+
+bouton.addEventListener('click', function() 
+{
+    mot.innerHTML = 'Bonjour !';
+});
