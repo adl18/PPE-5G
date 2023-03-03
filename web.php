bff35ef5e42d8e1e752de7ef3d29734c6e3f0b33
marinaattl <78730848+marinaattl@users.noreply.github.com>
2023-03-03 18:47:31 +0100

    Create web.php

---
+++ web.php
@@ -0,0 +1,25 @@
+<form action ="solution.php" method= "get" >
+    <label for="localisation"> Localisation:</label>
+    <input type="text" name="localisation" required maxlength="40" size="40"/>
+    <br>
+    <label for="utilisation"> Motif d'utilisation:</label>
+    <input type="text" name="utilisation" required maxlength="40" size="40" />
+    <br>
+    <label for="nom"> Nom de l'entreprise:</label>
+    <input type="text" name="nom" required maxlength="40" size="40" />
+    <br>
+    <label for="densite"> Densité de la zone:</label>
+    <input type="text" name="densite" required maxlength="40" size="40" />
+    <br>
+    <label for="surface"> Surface à couvrir:</label>
+    <input type="text" name="surface" required maxlength="40" size="40" />
+    <br>
+    <label for="latence"> Latence:</label>
+    <input type="text" name="latence" required maxlength="40" size="40" />
+    <br>
+    <label for="debit"> Débit:</label>
+    <input type="text" name="debit" required maxlength="40" size="40" />
+    <br>
+    <input type ="submit" value="valider" />
+
+</form>
