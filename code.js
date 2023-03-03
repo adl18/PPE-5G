a848d4fb5f11eb5568ac2e278e631392b5b860a9
marinaattl <78730848+marinaattl@users.noreply.github.com>
2023-03-03 10:12:42 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 //let numberOfDogs = 4; //créer une variable
 let debit = document.getElementById('debit');
 let mot = document.getElementById('mot');
-
+//helloooo my girl on est le 03/03/2023
 console.log('coucou');
 
 bouton.addEventListener('click', function() 
