b7eeeb5e6c39217955bb400170ef9ccef53bfe1b
marinaattl <78730848+marinaattl@users.noreply.github.com>
2023-03-03 18:48:04 +0100

    Create solution.php

---
+++ solution.php
@@ -0,0 +1,14 @@
+localisation :<?php echo $_GET['localisation'] ?>
+<br>
+utilisation:<?php echo $_GET['utilisation'] ?>
+<br>
+nom:<?php echo $_GET['nom'] ?>
+<br>
+densite:<?php echo $_GET['densite'] ?>
+<br>
+surface:<?php echo $_GET['surface'] ?>
+<br>
+latence:<?php echo $_GET['latence'] ?>
+<br>
+debit: <?php echo $_GET['debit'] ?>
+<br>
