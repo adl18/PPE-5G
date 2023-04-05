4a38d905b3b538b8085ceb425718c90dcc11fca2
marinaattl <78730848+marinaattl@users.noreply.github.com>
2023-04-05 12:01:49 +0200

    Update web.php

--- web.php
+++ web.php
@@ -2,7 +2,6 @@
     <label for="localisation"> Localisation:</label>
     <input type=radio id=choix1 name=choix value=1> Zone rurale
     <input type=radio id=choix2 name=choix value=2 > Zone urbaine
-    <input type=radio id=choix3 name=choix value=3> Entreprise
     <br>
     <label for="utilisation"> Motif d'utilisation:</label>
     <input type="text" name="utilisation" required maxlength="40" size="40" />
@@ -15,13 +14,13 @@
     <input type=radio id=choix5 name=dense value=2> Densité forte
     <br>
     <label for="surface"> Surface à couvrir en m2:</label>
-    <input type="text" name="surface" pattern="\d+" required />
+    <input type="number" step="0.01" min="0" name="surface" pattern="\d+" required />
     <br>
     <label for="latence"> Latence en milliseconde:</label>
-    <input type="text" name="latence"pattern="\d+" required/>
+    <input type="number" step="0.01" min="0" name="latence"pattern="\d+" required/>
     <br>
     <label for="debit"> Débit en Gb/s:</label>
-    <input type="text" name="debit"pattern="\d+" required/>
+    <input type="number" step="0.01" min="0" name="debit"pattern="\d+" required/>
     <br>
     <input type ="submit" value="valider" />
     <br>
